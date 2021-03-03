f206bacba32411273c84151bdddb824895666b3e
Dawid Dahl <dawiddahl@gmail.com>
2021-03-03 21:01:10 +0100

    modified server.ts path

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static("dist"));
 
 	app.get("/*", (req, res) => {
-		res.sendFile(path.join(__dirname, "dist", "index.html"));
+		res.sendFile(path.join(__dirname, "index.html"));
 	});
 }
 
